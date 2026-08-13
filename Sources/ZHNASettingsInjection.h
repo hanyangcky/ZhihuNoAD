38e15fc63534ed6951fc6d116d5cdcf48405f31f
hanyangcky <112472351+hanyangcky@users.noreply.github.com>
2026-08-13 11:29:49 +0800

    fix(连续阅读): JSON 清洗保护真实内容 + 设置页注入入口行 (v1.1.2)

---
+++ Sources/ZHNASettingsInjection.h
@@ -0,0 +1,12 @@
+//
+//  ZHNASettingsInjection.h
+//  在知乎 App 自带的"设置"页里插入一行本插件的设置入口
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+void ZHNAInstallSettingsInjection(void);
+
+NS_ASSUME_NONNULL_END
